89e3667f659b89a0eda25171b7f20436cd3b18d8
ali <aliderraj00@gmail.com>
2025-04-19 14:40:35 +0100

    ex01 done

--- Module06/ex01/main.cpp
+++ Module06/ex01/main.cpp
@@ -3,7 +3,6 @@
 int main() {
   Data object;
   
-  // uintptr_t serialized = Serializer::serialize(&object);
   std::cout << &object << std::endl;
   Data* ptr = Serializer::deserialize(Serializer::serialize(&object));
   std::cout << ptr << std::endl;

032a189c2fcc9caf78dbd68ba7d750b93ecded5e
ali <aliderraj00@gmail.com>
2025-04-22 18:19:10 +0100

    ADD const to function ptr arg

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -5,7 +5,7 @@
 #include <string>
 
 template <typename T>
-void  iter(T *arr, size_t size, void (*f)(T&)) {
+void  iter(T *arr, size_t size, void (*f)(T const &)) {
   for (size_t i = 0; i < size; i++)
     f(arr[i]);
 }
